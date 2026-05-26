0cbdc83ccd07f75d1e6e457c2213d1eb1c63110f
Tunasisme9781 <ngoctuan186205@gmail.com>
2026-05-26 09:12:19 +0700

    Update Cuahang.sql

--- Cuahang.sql
+++ Cuahang.sql
@@ -2,8 +2,8 @@ create table products(
                          id serial primary key,
                          name varchar(50) ,
                          category varchar(50),
-                         price decimal(10,2),
-                         stock int
+                         price decimal(10,2) check(price > 0),
+                         stock int check(stock >=0)
 );
 insert into products(name, category, price, stock) values
                                                        ('Laptop Dell', 'Electronics', 1500.00, 5),
@@ -11,17 +11,35 @@ insert into products(name, category, price, stock) values
                                                        ('Bàn phím Razer', 'Electronics', 120.00, 20),
                                                        ('Tủ lạnh LG', 'Home Appliances', 800.00, 3),
                                                        ('Máy giặt Samsung', 'Home Appliances', 600.00, 2);
+--1
 insert into products(name, category, price, stock) values
     ('Điều hòa Panasonic','Home Appliances',400.00,10);
+
+--2
 update products
 set stock = 7
 where name = 'Laptop Dell';
+
+--3
 delete from products where stock=0;
-select *from products  order by price asc ;
+
+--4
+select name,price
+from products  order by price asc ;
+
+--5
 select distinct category from products;
+
+--6
 select name,price from products where price between 100 and 1000;
-select name from products where name like '%LG%' or name like '%Samsung%';
+
+--7
+select name from products where name ilike '%LG%' or name ilike '%Samsung%';
+
+--8a
 select name,price from products order by price desc limit 2;
+
+--8b
 select name,price from products order by id asc limit 2 offset 1;
 
-select *from products;
+
